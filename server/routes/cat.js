add95c75602e85a0ee76c36980be21a2866e6e13
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-17 13:13:18 -0500

    setup add

--- server/routes/cat.js
+++ server/routes/cat.js
@@ -1,7 +1,9 @@
 "use strict"
 const express = require('express')
 const { resp } = require('../components/response')
+const { OurSQL } = require('../components/oursql')
 let router = express.Router()
+let sql = new OurSQL()
 
 router.get('/', (req, res) => {
     return res.json(resp.make()
@@ -11,9 +13,25 @@ router.get('/', (req, res) => {
 })
 
 router.get('/add', (req, res) => {
-    return res.json(resp.make()
-
-    )
+    let post = {
+        
+    }
+    sql.query('INSERT INTO Cat SET ?', post, 
+    (results, fields) => {
+        sql.quitConnection()
+        return res.json(resp.make()
+            .setMessage("Query successful!")
+            .setResponseCode(200)
+        )
+    }, (error) => {
+        if (error){
+            return res.json(resp.make()
+                .setError(error)
+                .setResponseCode(500)
+                .setMessage("There was an error :(")
+            )
+        }
+    })
 })
 
 router.get('/edit', (req, res) => {
