fca68e77b654588328fe3d9004117b72a079d6b9
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-17 18:37:06 -0500

    change

--- server/src/Create/Cat.sql
+++ server/src/Create/Cat.sql
@@ -7,12 +7,8 @@ CREATE TABLE IF NOT EXISTS Cat(
     breed       VARCHAR(64),
     behavior    VARCHAR(256),
     pair        INT,
-<<<<<<< HEAD
     stat        BIT DEFAULT 0,
     locId       INT NOT NULL,
-=======
-    stat        BIT CHECK (fixed IN (1, 0, NULL)) DEFAULT 0,
->>>>>>> e0971fc4b7c44235633d1e39b0ad188ac3440201
     adopterId   INT,
     PRIMARY KEY(id)
 );
