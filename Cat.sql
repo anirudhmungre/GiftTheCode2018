c716100b027f6aaeff677a232d3e4c49e2533b28
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-17 02:11:50 -0500

    create cat table

---
+++ Cat.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS Cat(
+    id          INT NOT NULL,
+    name        VARCHAR(64),
+    dob         DATE,
+    intakeDate  DATE,
+    fostDate    DATE,
+    sex         CHAR(1),
+    breed       VARCHAR(64),
+    behavior    VARCHAR(256),
+    outcome     VARCHAR(64),
+    healthId    INT NOT NULL,
+    locId       INT NOT NULL
+);
