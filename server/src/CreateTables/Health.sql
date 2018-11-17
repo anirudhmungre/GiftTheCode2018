95e9011d798a86bcc7c3027e2754ea27b7aa40c7
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-17 03:57:21 -0500

    server

--- server/src/CreateTables/Health.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Health(
-    id              INT NOT NULL,
-    fivTested       BIT CHECK (fivTested IN (1, 0, NULL)),
-    flvTested       BIT CHECK (flvTested IN (1, 0, NULL)),
-    fvrcpVacDate    DATE,
-    rabiesVacDate   DATE,
-    medNotes        VARCHAR(256),
-    PRIMARY KEY (id)
-);
--- server/src/CreateTables/Location.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Location(
-    id          INT NOT NULL,
-    addr        VARCHAR(128),
-    typ         VARCHAR CHECK (type IN ('Foster Home', 'Vet', 'Store', 'Shelter', 'Volunteer Centre', 'Adopted'))
-    inDate      DATE,
-    outDate     DATE,
-    PRIMARY KEY(id)
-);
