e0200cf3ec30e581868dfba133a536af27dd6a54
Ethan Elliott <ethan.elliott@hotmail.ca>
2018-11-18 11:55:34 -0500

    cat info

--- torontocatrescue/src/views/CatInfo.vue
+++ torontocatrescue/src/views/CatInfo.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="about">
-    <md-card class="md-elevation-5">
-      <md-card-header>
-        <div class="md-title">Cat Info</div>
-      </md-card-header>
+  <div class="catinfo">
+    <md-card class="md-card-example md-elevation-5">
+      <md-card-area md-inset>
+        <!-- <md-card-media md-ratio="16:9">
+          <img src="/assets/examples/card-example.jpg" alt="Cat">
+        </md-card-media> -->
+        <md-card-header>
+          <h2 class="md-title">{{ cata.cName }}</h2>
+          <div class="md-subhead">
+            <md-icon>event</md-icon>
+            <span>{{ (new Date(cata.dob).toLocaleDateString()) }}</span>
+          </div>
+          <div class="md-subhead">
+            <md-icon>pets</md-icon>
+            <span>{{ cata.breed }}</span>
+          </div>
+          <div class="md-subhead">
+            <md-icon>hdr_weak</md-icon>
+            <span>{{ cata.sex }}</span>
+          </div>
+        </md-card-header>
+        <md-card-content>
+          {{ cata.behavior }}
+        </md-card-content>
+      </md-card-area>
       <md-card-content>
-        {{ id }}
       </md-card-content>
     </md-card>
   </div>
@@ -17,12 +36,18 @@ import axios from 'axios'
 export default {
   name: 'CatInfo',
   props: ['id'],
+  data: function() {
+    return {
+      cata: null
+    }
+  },
   mounted: function() {
     axios
     .post("/cat/gmayg", {
       catId: this.id
     })
     .then(response => {
+      this.cata = response.data.data
       console.log(response)
     })
   }
