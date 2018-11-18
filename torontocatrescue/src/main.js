a880cf18ece37067ceb907773c3d95a859c571a0
Ethan Elliott <ethan.elliott@hotmail.ca>
2018-11-18 00:02:20 -0500

    lots of stuff

--- torontocatrescue/src/main.js
+++ torontocatrescue/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import 'vue-material/dist/vue-material.min.css'
+
+axios.defaults.baseURL = 'http://10.0.79.109:1337'
 
 Vue.config.productionTip = false
 
